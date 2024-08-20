1e4e0150453a7edbb70f29fbe17257b7658a7301
Teemu Taivainen <teemu.taivainen@aalto.fi>
2024-08-20 23:45:51 +0300

    Add configuration to app

--- backend/server/__init__.py
+++ backend/server/__init__.py
@@ -1,14 +1,17 @@
 """
 This module contains the Flask application factory.
 """
+
 from flask import Flask
 from . import views
+
 
 def create_app():
     """
     Create a Flask application instance.
     """
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_pyfile("config.py")
     # Register the api blueprint that contains the views (route handlers).
     app.register_blueprint(views.api_bp)
 

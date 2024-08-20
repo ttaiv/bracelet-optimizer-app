7c1923ad4b0ce7b0c0045f5cbddaca27e0d467b7
Teemu Taivainen <teemu.taivainen@aalto.fi>
2024-08-20 23:45:25 +0300

    Add validation for solve request

---
+++ backend/server/validation.py
@@ -0,0 +1,19 @@
+import json
+from jsonschema import validate
+
+solve_schema = {
+    "type": "object",
+    "properties": {
+        "letter_counts": {
+            "type": "object",
+            "additionalProperties": {"type": "integer"},
+            "propertyNames": {"type": "string"},
+        },
+        "texts": {"type": "array", "items": {"type": "string"}},
+    },
+    "required": ["letter_counts"],
+}
+
+
+def validate_solve(data):
+    validate(data, solve_schema)

8c62a111f3a8baf0f8d234cfb361bbe6cd699784
Teemu Taivainen <teemu.taivainen@aalto.fi>
2024-08-20 23:46:31 +0300

    Implement views for routes /solve and /texts

--- backend/server/views.py
+++ backend/server/views.py
@@ -2,10 +2,34 @@
 This module contains the views (route handlers).
 The routes are associated with the api blueprint.
 """
-from flask import Blueprint
+
+from flask import Blueprint, request, jsonify, current_app
+from .bracelet_texts_optimizer import find_best_texts_ilp
+from .validation import validate_solve
+from .utils import preprocess_texts
 
 api_bp = Blueprint("api", __name__, url_prefix="/api")
 
-@api_bp.route("/solve")
-def index():
-    return "Hello, index!"
+
+@api_bp.route("/solve", methods=["POST"])
+def solve_best_texts():
+    data: dict = request.get_json()
+    validate_solve(data)  # Will raise an error if the data is invalid
+
+    letter_counts: dict[str, int] = data["letter_counts"]
+    texts: list[str] = data.get("texts", current_app.config["DEFAULT_TEXTS"])
+    preprocessed_texts = preprocess_texts(texts)
+
+    # Solve the problem
+    minimized_letter_count, best_texts = find_best_texts_ilp(
+        letter_counts, preprocessed_texts, print_output=False
+    )
+
+    return jsonify(
+        {"minimized_letter_count": minimized_letter_count, "best_texts": best_texts}
+    )
+
+
+@api_bp.route("/texts", methods=["GET"])
+def get_default_texts():
+    return jsonify(current_app.config["DEFAULT_TEXTS"])

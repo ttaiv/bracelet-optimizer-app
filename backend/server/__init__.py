17ac7118850308a65bdc21234ffa84429ba5eb21
Teemu Taivainen <teemu.taivainen@aalto.fi>
2024-08-22 13:36:05 +0300

    Move config file to backend root

--- backend/server/__init__.py
+++ backend/server/__init__.py
@@ -2,6 +2,7 @@
 This module contains the Flask application factory.
 """
 
+from pathlib import Path
 from flask import Flask
 from . import views
 
@@ -10,8 +11,11 @@ def create_app():
     """
     Create a Flask application instance.
     """
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_pyfile("config.py")
+    app = Flask(__name__)
+    # Load config from backend/config.py
+    config_file = Path(__file__).parent.parent / "config.py"
+    app.config.from_pyfile(config_file)
+
     # Register the api blueprint that contains the views (route handlers).
     app.register_blueprint(views.api_bp)
 

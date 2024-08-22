5f4f5ebf739a40da6c2123dc8879ca13d3f74c7c
Teemu Taivainen <teemu.taivainen@aalto.fi>
2024-08-22 12:02:37 +0300

    Add docstrings to views

--- backend/server/views.py
+++ backend/server/views.py
@@ -13,6 +13,13 @@ api_bp = Blueprint("api", __name__, url_prefix="/api")
 
 @api_bp.route("/solve", methods=["POST"])
 def solve_best_texts():
+    """
+    Solve the bracelet texts problem for the the letter counts and texts provided in the request.
+    If no texts are provided, the default texts are used.
+
+    Returns:
+        Response: JSON response containing the minimized letter count and the best texts.
+    """
     data: dict = request.get_json()
     validate_solve(data)  # Will raise an error if the data is invalid
 
@@ -32,4 +39,10 @@ def solve_best_texts():
 
 @api_bp.route("/texts", methods=["GET"])
 def get_default_texts():
+    """
+    Get the default texts.
+
+    Returns:
+        Response: JSON response containing the default texts.
+    """
     return jsonify(current_app.config["DEFAULT_TEXTS"])

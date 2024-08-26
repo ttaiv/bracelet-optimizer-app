0c0c071ed9d66d64488e49d0beddf5e55045a7b1
Teemu Taivainen <136599271+ttaiv@users.noreply.github.com>
2024-08-26 20:32:26 +0300

    Add prettier for formatting (#4)
    * Prepare eslint config for prettier
    
    * Install prettier
    
    * Run prettier
    
    * Add npm scripts for formatting
    
    * Add prettier format checking to linting workflow

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,14 @@
 import { getDefaultTexts } from "./api";
 
 export default async function Home() {
-
   const texts = await getDefaultTexts();
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <p>I requested the API for default texts used in the optimizing and got the following texts:</p>
+        <p>
+          I requested the API for default texts used in the optimizing and got
+          the following texts:
+        </p>
         <br />
         <ul>
           {texts.map((text, index) => (
